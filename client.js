cff36dab2a380b8ca1e70fb1924b18a2af898f45
edeity <edeity.fly@gmail.com>
2019-01-02 21:55:24 +0800

    最简单的功能

--- client.js
+++ client.js
@@ -1,39 +1,79 @@
 const dgram = require('dgram');
 const ioHook = require('iohook');
 
-ioHook.on("mousemove", event => {
-    console.log(event);
-    /* prints :
-     {
-     type: 'mousemove',
-     x: 700,
-     y: 400
-     }
-     */
-});
-ioHook.start();
+const config = {
+    ip: '10.13.145.53',
+    port: 41234
+}
 
-// 监听键盘输入
-var stdin = process.stdin;
-stdin.setRawMode( true );
-stdin.resume();
-stdin.setEncoding( 'utf8' );
-stdin.on('data', function( key ){
-    if ( key === '\u0003' ) {
-        process.exit();
-    }
+const EVENT_TYPE =  {
+    MOUSE_MOVE: 0,
+    MOUSE_CLICK: 2,
+    MOUSE_WHEEL: 3,
+    KEY_DOWN: 1,
+}
 
-    // process.stdout.write( key );
-
-    const message = Buffer.from(key);
-
+function send(obj) {
     // 创建udp报文
+    const message = Buffer.from(JSON.stringify(obj));   
     const client = dgram.createSocket('udp4');
-    client.send(message, 41234, '172.26.149.1', (err) => {
+    client.send(message, config.port, config.ip, (err) => {
         client.close();
+    });
+}
+
+ioHook.on("mouseclick", event => {
+    send({
+        c: EVENT_TYPE.MOUSE_CLICK,
+        b: event.button,
+    })
+})
+
+ioHook.on("mousewheel", event => {
+    send({
+        c: EVENT_TYPE.MOUSE_WHEEL,
+        a: event.amount,
+        d: event.direction === 3 ? 1 : 0
+    })
+})
+
+ioHook.on("mousemove", event => {
+    send({
+        c: EVENT_TYPE.MOUSE_MOVE, 
+        p: {
+            x: event.x,
+            y: event.y
+        }
     });
 });
 
-// myEmitter.emit('mouseMove')
-// var robot = require("robotjs")
-// robot.moveMouseSmooth(100, 100)
+ioHook.on("keydown", event => {
+    let msg = {
+        c: EVENT_TYPE.KEY_DOWN,
+        k: {
+            k: event.keycode,
+        }
+    };
+    if (event.altKey) msg.k.a = 1;
+    if (event.shiftKey) msg.k.s = 1;
+    if (event.ctrlKey) msg.k.c = 1;
+    if (event.metaKey)msg.k.m = 1;
+    send(msg);
+})
+
+ioHook.start();
+
+// 监听键盘输入
+// var stdin = process.stdin;
+// stdin.setRawMode( true );
+// stdin.resume();
+// stdin.setEncoding( 'utf8' );
+// stdin.on('data', function( key ){
+//     if (key === '\u0003') {
+//         process.exit();
+//     }
+//     send({
+//         c: EVENT_TYPE.KEYBOAR,
+//         k: key
+//     })
+// });
